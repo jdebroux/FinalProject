dd02153c6be5afe0f8ad8cee2efe4e48c3821cdd
JHowell78 <jahowell1978@gmail.com>
2019-10-17 12:00:50 -0600

    navbar testing

--- ToolDepot/src/main/java/com/skilldistillery/tooldepotapp/services/UserService.java
+++ ToolDepot/src/main/java/com/skilldistillery/tooldepotapp/services/UserService.java
@@ -1,5 +1,21 @@
 package com.skilldistillery.tooldepotapp.services;
 
+import java.util.List;
+
+import com.skilldistillery.tooldepotapp.entities.User;
+
 public interface UserService {
+
+	User findById(int id);
+
+	List<User> findByUserName(String keyword);
+
+	List<User> findAll();
+
+	User update(User user, int id);
+
+	User create(User user);
+
+	Boolean delete(int id);
 
 }
