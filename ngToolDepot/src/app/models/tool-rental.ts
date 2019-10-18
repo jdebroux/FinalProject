40cab5cc23f17410d375e302da31ce74c2b17de0
jdebroux <jtdebroux@gmail.com>
2019-10-18 10:27:41 -0600

    ng models finished

--- ngToolDepot/src/app/models/tool-rental.ts
+++ ngToolDepot/src/app/models/tool-rental.ts
@@ -1,2 +1,30 @@
 export class ToolRental {
+  id: number;
+  checkout: string;
+  returned: string;
+  totalCost: number;
+  createDate: string;
+  updateDate: string;
+  tool: object;
+  renter: object;
+
+  constructor(
+    id?: number,
+    checkout?: string,
+    returned?: string,
+    totalCost?: number,
+    createDate?: string,
+    updateDate?: string,
+    tool?: object,
+    renter?: object
+  ) {
+    this.id = id;
+    this.checkout = checkout;
+    this.returned = returned;
+    this.totalCost = totalCost;
+    this.createDate = createDate;
+    this.updateDate = updateDate;
+    this.tool = tool;
+    this.renter = renter;
+  }
 }
