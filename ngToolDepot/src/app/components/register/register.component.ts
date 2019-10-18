5352fafa9f1da52d20b41bafc0f1dc617f661215
Cody Fulker <codyfulker@SDMacBookPro3-2.local>
2019-10-18 08:42:12 -0600

    Footer component, color scheme updated for footer and login dropdown

--- ngToolDepot/src/app/components/register/register.component.ts
+++ ngToolDepot/src/app/components/register/register.component.ts
@@ -10,7 +10,7 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
